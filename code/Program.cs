1a45c3b9ced81acc62e036fdc8b3ae00a77d159a
James <james.m.peachey@gmail.com>
2020-02-26 13:34:24 +0000

    code

---
+++ code/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace code
+{
+
+
+class Countries
+    {
+        private string Name;
+        private int Pop;
+        private bool EU;
+
+        public Countries()
+        {
+            Name = "Default";
+            Pop = 0;
+            EU = false;
+        }
+        public Countries(string name, int pop, bool eu)
+        {
+            Name = name;
+            Pop = pop;
+            EU = eu;
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
