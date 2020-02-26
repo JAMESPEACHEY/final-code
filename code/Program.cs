791b8368c7ece5ff57c36055793cd715a749f141
James <james.m.peachey@gmail.com>
2020-02-26 14:23:54 +0000

    Finished countries

--- code/Program.cs
+++ code/Program.cs
@@ -36,23 +36,23 @@ namespace code
                 Countries Denmark = new Countries("Denmark", 5603000, false);
                 Countries Estonia = new Countries("Estonia", 1328000, true);
                 Countries Finland = new Countries("Finland", 5513000, true);
-                Countries Germany = new Countries("Germany", 1328000, true);
-                Countries Greece = new Countries("Greece", 1328000, true);
-                Countries Hungary = new Countries("Hungary", 1328000, true);
-                Countries Ireland = new Countries("Ireland", 1328000, true);
-                Countries Italy = new Countries("Italy", 1328000, true);
-                Countries Latvia = new Countries("Latvia", 1328000, true);
-                Countries Lithuania = new Countries("Lithuania", 1328000, true);
-                Countries Luxembourg = new Countries("Luxembourg", 1328000, true);
-                Countries Malta = new Countries("Malta", 1328000, true);
-                Countries Netherlands = new Countries("Netherlands ", 1328000, true);
-                Countries Poland = new Countries("Poland ", 1328000, true);
-                Countries Portugal = new Countries("Portugal ", 1328000, true);
-                Countries Romania = new Countries("Romania ", 1328000, true);
-                Countries Slovakia = new Countries("Slovakia ", 1328000, true);
-                Countries Slovenia = new Countries("Slovenia ", 1328000, true);
-                Countries Spain = new Countries("Spain ", 1328000, true);
-                Countries Sweden = new Countries("Sweden ", 1328000, true);
+                Countries Germany = new Countries("Germany", 82790000, true);
+                Countries Greece = new Countries("Greece", 10740000, true);
+                Countries Hungary = new Countries("Hungary", 9773000, true);
+                Countries Ireland = new Countries("Ireland", 4830000, true);
+                Countries Italy = new Countries("Italy", 60480000, true);
+                Countries Latvia = new Countries("Latvia", 1920000, true);
+                Countries Lithuania = new Countries("Lithuania", 2794000, true);
+                Countries Luxembourg = new Countries("Luxembourg", 602005000, true);
+                Countries Malta = new Countries("Malta", 493559, true);
+                Countries Netherlands = new Countries("Netherlands ", 17180000, true);
+                Countries Poland = new Countries("Poland ", 37980000, true);
+                Countries Portugal = new Countries("Portugal ", 1029000, true);
+                Countries Romania = new Countries("Romania ", 19530000, true);
+                Countries Slovakia = new Countries("Slovakia ", 5450000, true);
+                Countries Slovenia = new Countries("Slovenia ", 2067000, true);
+                Countries Spain = new Countries("Spain ", 46660000, true);
+                Countries Sweden = new Countries("Sweden ", 10120000, true);
 
             }
 
