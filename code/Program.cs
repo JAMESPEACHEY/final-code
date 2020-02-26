6951817a9f274958b13428c451a77a750a319d98
Rio <riowright@hotmail.co.uk>
2020-02-26 14:00:14 +0000

    Added Seclection Code

--- code/Program.cs
+++ code/Program.cs
@@ -27,6 +27,29 @@ namespace code
             static void Main(string[] args)
             {
                 Countries France = new Countries("france", 1000, true);
+
+            }
+            public void Selection()
+            {
+                Console.WriteLine("Are you incluiding all countries or just the Eurozone countries? (All/Euro)");
+                string answer = "";
+                retry:
+                answer = Console.ReadLine().ToUpper();
+                if (answer == "ALL")
+                {
+
+                }
+                else if (answer == "EURO")
+                {
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Your Answer Was Not Valid! Try again");
+                    goto retry;
+                }
+
             }
         }
     }
